41890352b90aac1f14df60692274847642c23ae6
Sandra Hufford <flourmill96@gmail.com>
2022-11-29 15:30:37 -0500

    added announcement

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="flex bg-blue-800 py-10 border-t-gray-500 border-t-8">
-    <div class="flex w-1/2 justify-center"> <!--Announcements-->
-      <h1 class="mt-4 text-3xl text-white">Annual Meeting</h1>
+  <div class="md:flex bg-blue-800 py-10 border-t-gray-500 border-t-8">
+    <div class="sm:w-full md:w-1/2 md:justify-center"> <!--Announcements-->
+      <h1 class="mt-4 text-3xl text-white text-center">Annual Meeting</h1>
+      <p class="text-white text-xl text-center">Dec 8, 2022 ~  6:30 pm</p>
+      <p class="text-white text-xl text-center">50 E North Street</p>
     </div> 
 
     <div class="flex justify-end">
