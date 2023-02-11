29d30b283ddd564d521bd8213f5647a85414cf5e
Sandra Hufford <flourmill96@gmail.com>
2023-02-11 14:44:20 -0500

    changed phone number

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,7 +2,7 @@
     <div class="w-3/4 h-48 mx-auto border-blue-500">
         <h1 class="text-2xl text-bold text-center underline">Contact Us</h1>
     <br>
-        <p class="text-center text-xl">(765) 379-2998</p>
+        <p class="text-center text-xl">(765) 379-2999</p>
         <p class="text-center text-xl">rvas@geetel.net</p>
     </div>
 </template>
